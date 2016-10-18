793ddbc690106eaa5387050df0fc7def121471e9
Matt Sadauckas <matthewsadauckas@gmail.com>
2016-10-17 20:05:15 -0600

    feat(core) Add intitial i/o; data connectivity WIP

--- js/apiCall.js
+++ js/apiCall.js
@@ -1,1 +1,33 @@
 //apiCall.js
+
+/* https://open.whitehouse.gov/ data from the
+   Climate Change Adaptation Task Force
+*/
+
+var ourData = {};
+
+window.onload = function getDataSets () {
+
+  let urlNP = 'https://open.whitehouse.gov/resource/qswu-k8nh.json'
+  // let urlStates = https://gist.github.com/matsad3547/f1edc675c7ede3f808f746747d3cbfa6.js
+
+  // over 27,000 hits with the following
+  // $.get(url, function(data) {
+  //   console.log(data);
+  // });
+
+  // we'll do the long version to prevent overload
+  $.ajax({
+    dataType: "json",
+    url: urlNP,
+    data: {
+      "$limit" : 100
+    }
+  }).done(function(data) {
+    ourData = data;
+    // getData(ourData);
+  })
+
+
+
+};
--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
 // main.js
+
+function getData () {
+  return new Promise(function (resolve, reject) {
+  setTimeout( function() {
+     if (ourData) {
+       return dataset
+      } else theThing();
+    }
+  } 2000)
+}
+
+console.log('We are up and running!');
+
+// let stateNpData =
+
+function getData() {
+  console.log(ourData);
+  return ourData
+}
+// console.log(stateNpData);
+
+document.querySelector('button').onclick = searchStates;
+// document.querySelector('.searchBox').onsubmit = searchStates;
+
+function searchStates () {
+  let input = document.querySelector('input').value;
+  display (input);
+  console.log(input);
+}
+
+function display (input) {
+  document.querySelector('.numImprov').innerHTML = input;
+}
+
+function stateID(stateName) {
+  return states_hash[stateName];
+}
+
+let stateName = 'Idaho';
+console.log(stateID(stateName));
