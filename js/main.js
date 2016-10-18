025b32815723798dc9f57ba8de97d0db7eb6e657
Matt Sadauckas <matthewsadauckas@gmail.com>
2016-10-17 23:06:42 -0600

    feat(core) Improve data connectivity; Improve function structure

--- js/main.js
+++ js/main.js
@@ -1,31 +1,71 @@
 // main.js
 
-function getData () {
-  return new Promise(function (resolve, reject) {
-  setTimeout( function() {
-     if (ourData) {
-       return dataset
-      } else theThing();
-    }
-  } 2000)
-}
+var dataProcessing = (function (){
+  var data = {};
 
 console.log('We are up and running!');
 
-// let stateNpData =
+function dataLoad () {
+  return new Promise( function(resolve, reject) {
+    setTimeout( function() {
+      if (ourData) {
+        resolve('got it');
+      }
+      else {
+        reject('no go');
+      }
+    }, 2000);
+  })
+};
 
-function getData() {
-  console.log(ourData);
-  return ourData
-}
-// console.log(stateNpData);
+var loaded = dataLoad();
+
+loaded.then(function(result) {
+  // console.log(ourData);
+  writeObject(ourData);
+  }, function(err) {
+    console.log(err + ': Try again');
+});
+
+console.log(data);
+
+// function getData() {
+//   writeObject(ourData);
+//   // console.log(ourData);
+//   console.log(ourData[0].national_parks_being_improved);
+//   return ourData;
+// };
+
+function writeObject (arr){
+  arr.map(transcribeData)
+};
+
+function transcribeData (obj) {
+  data = {
+    projects: obj.national_parks_being_improved,
+    state: obj.state
+  }
+  console.log(data);
+  return data;
+};
+
+//TODO: Define a method (within data???) for getting at the objects - need a promise???
+
+//   function getNpProjects (stateAbvr) {
+//     let numProjects = this.national_parks_being_improved;
+//   }
+//   return {
+//     getNumProjects: getNpProjects
+//   }
+// })();
 
 document.querySelector('button').onclick = searchStates;
-// document.querySelector('.searchBox').onsubmit = searchStates;
 
 function searchStates () {
   let input = document.querySelector('input').value;
-  display (input);
+  let stateAbvr = stateID(input)
+  // let msg = input + ' has ' + num + 'National Park improvement projects';
+  display (stateAbvr);
   console.log(input);
 }
 
@@ -39,3 +79,5 @@ function stateID(stateName) {
 
 let stateName = 'Idaho';
 console.log(stateID(stateName));
+
+})();
