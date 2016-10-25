361d275b72bf67d2cd629fa869c16f14b241dd69
Matt Sadauckas <matthewsadauckas@gmail.com>
2016-10-25 09:10:09 -0600

    feat(core) Update state coordinates

--- js/states_hash.js
+++ js/states_hash.js
@@ -12,7 +12,7 @@ var states_hash = [
   {
     state: 'Arizona',
     abvr: 'AZ',
-    coordinates: [34.3, -152.3]
+    coordinates: [34.3, -111.7]
   },
   {
     state:'Arkansas',
@@ -42,22 +42,22 @@ var states_hash = [
   {
     state: 'Florida',
     abvr: 'FL',
-    coordinates: [28.4, -82.3]
+    coordinates: [28.6, -82.4]
   },
   {
     state: 'Georgia',
     abvr: 'GA',
-    coordinates: [32.4, -83.3]
+    coordinates: [32.6, -83.4]
   },
   {
     state: 'Hawaii',
     abvr: 'HI',
-    coordinates: [64.4, -152.3]
+    coordinates: [20.3, -156.4]
   },
   {
     state: 'Idaho',
     abvr: 'ID',
-    coordinates: [64.4, -152.3]
+    coordinates: [44.4, -114.6]
   },
   {
     state: 'Illinois',
@@ -67,186 +67,186 @@ var states_hash = [
   {
     state: 'Indiana',
     abvr: 'IN',
-    coordinates: [40.5, -86.5]
+    coordinates: [40, -86.3]
   },
   {
     state: 'Iowa',
     abvr: 'IA',
-    coordinates: [64.4, -152.3]
+    coordinates: [42, -93.5]
   },
   {
     state: 'Kansas',
     abvr: 'KS',
-    coordinates: [64.4, -152.3]
+    coordinates: [38.5, -98.4]
   },
   {
     state: 'Kentucky',
     abvr: 'KY',
-    coordinates: [64.4, -152.3]
+    coordinates: [37.5, -85.3]
   },
   {
     state: 'Louisiana',
     abvr: 'LA',
-    coordinates: [64.4, -152.3]
+    coordinates: [31, -92]
   },
   {
     state: 'Maine',
     abvr: 'ME',
-    coordinates: [64.4, -152.3]
+    coordinates: [45.4, -69.2]
   },
   {
     state: 'Maryland',
     abvr: 'MD',
-    coordinates: [64.4, -152.3]
+    coordinates: [39, -76.8]
   },
   {
     state: 'Massachusetts',
     abvr: 'MA',
-    coordinates: [42.3, -72]
+    coordinates: [42.3, -71.8]
   },
   {
     state: 'Michigan',
     abvr: 'MI',
-    coordinates: [64.4, -152.3]
+    coordinates: [41.3, -85.4]
   },
   {
     state: 'Minnesota',
     abvr: 'MN',
-    coordinates: [64.4, -152.3]
+    coordinates: [46.3, -94.3]
   },
   {
     state: 'Mississippi',
     abvr: 'MS',
-    coordinates: [64.4, -152.3]
+    coordinates: [32.7, -89.7]
   },
   {
   state: 'Missouri',
   abvr: 'MO',
-  coordinates: [64.4, -152.3]
+  coordinates: [38.4, -92.5]
   },
   {
   state: 'Montana',
   abvr: 'MT',
-  coordinates: [64.4, -152.3]
+  coordinates: [47, -109.6]
   },
   {
   state: 'Nebraska',
   abvr: 'NE',
-  coordinates: [41.5, -100]
+  coordinates: [41.5, -99.8]
   },
   {
   state: 'Nevada',
   abvr: 'NV',
-  coordinates: [64.4, -152.3]
+  coordinates: [39.3, -116.6]
   },
   {
   state: 'New Hampshire',
   abvr: 'NH',
-  coordinates: [64.4, -152.3]
+  coordinates: [43.7, -71.6]
   },
   {
   state: 'New Jersey',
   abvr: 'NJ',
-  coordinates: [40, -22.5]
+  coordinates: [40, -74.7]
   },
   {
   state: 'New Mexico',
   abvr: 'NM',
-  coordinates: [64.4, -152.3]
+  coordinates: [34.4, -106.1]
   },
   {
   state: 'New York',
   abvr: 'NY',
-  coordinates: [64.4, -152.3]
+  coordinates: [43, -75.5]
   },
   {
   state: 'North Carolina',
   abvr: 'NC',
-  coordinates: [64.4, -152.3]
+  coordinates: [35.6, -79.4]
   },
   {
   state: 'North Dakota',
   abvr: 'ND',
-  coordinates: [64.4, -152.3]
+  coordinates: [47.5, -100.5]
   },
   {
   state: 'Ohio',
   abvr: 'OH',
-  coordinates: [64.4, -152.3]
+  coordinates: [40.3, -82.8]
   },
   {
   state: 'Oklahoma',
   abvr: 'OK',
-  coordinates: [64.4, -152.3]
+  coordinates: [35.6, -97.5]
   },
   {
   state: 'Oregon',
   abvr: 'OR',
-  coordinates: [64.4, -152.3]
+  coordinates: [43.9, -120.6]
   },
   {
   state: 'Pennsylvania',
   abvr: 'PA',
-  coordinates: [64.4, -152.3]
+  coordinates: [40.9, -77.8]
   },
   {
   state: 'Rhode Island',
   abvr: 'RI',
-  coordinates: [42, -71]
+  coordinates: [41.7, -71.6]
   },
   {
   state: 'South Carolina',
   abvr: 'SC',
-  coordinates: [64.4, -152.3]
+  coordinates: [33.9, -80.9]
   },
   {
   state: 'South Dakota',
   abvr: 'SD',
-  coordinates: [64.4, -152.3]
+  coordinates: [44.4, -100.2]
   },
   {
   state: 'Tennessee',
   abvr: 'TN',
-  coordinates: [64.4, -152.3]
+  coordinates: [35.9, -86.4]
   },
   {
   state: 'Texas',
   abvr: 'TX',
-  coordinates: [31.3, -100]
+  coordinates: [31.5, -99.3]
   },
   {
   state: 'Utah',
   abvr: 'UT',
-  coordinates: [64.4, -152.3]
+  coordinates: [39.3, -111.7]
   },
   {
   state: 'Vermont',
   abvr: 'VT',
-  coordinates: [64.4, -152.3]
+  coordinates: [44.1, -72.7]
   },
   {
   state: 'Virginia',
   abvr: 'VA',
-  coordinates: [64.4, -152.3]
+  coordinates: [37.5, -78.9]
   },
   {
   state: 'Washington',
   abvr: 'WA',
-  coordinates: [64.4, -152.3]
+  coordinates: [47.4, -120.5]
   },
   {
   state: 'West Virginia',
   abvr: 'WV',
-  coordinates: [64.4, -152.3]
+  coordinates: [38.6, -80.6]
   },
   {
   state: 'Wisconsin',
   abvr: 'WI',
-  coordinates: [44.5, -90]
+  coordinates: [44.6, -90]
   },
   {
   state: 'Wyoming',
   abvr: 'WY',
-  coordinates: [64.4, -152.3]
+  coordinates: [43, -107.6]
   },
   ];
