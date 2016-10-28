b221c93ec0a272a15f48a5bb3eafbf08ebdaec19
Matt Sadauckas <matthewsadauckas@gmail.com>
2016-10-27 18:23:01 -0600

    feat(core) Add zoom and pan functionality; maps to separate file

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ var data_processing_module = (function() {
     let stateInf =  npImprovements.stateAbvr(srchInput);
     let projects = npImprovements.getNpProjects(stateInf.abvr, data);
     let msg = npImprovements.messager(srchInput, projects);
-    npImprovements.popupMsg(msg, stateInf.coords);
+    maps.popupMsg(msg, stateInf.coords);
     console.log(msg);
   }
 
@@ -34,22 +34,6 @@ var npImprovements = (function (){
   console.log('We are up and running!');
 
   document.querySelector('button').onclick = data_processing_module.clicked;
-
-var mymap = L.map('map').setView([50, -98.0], 3);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 50,
-    id: 'mapbox.outdoors',
-    accessToken: 'pk.eyJ1IjoibWF0c2FkIiwiYSI6ImNpdWZyMGp6ZTAwaHkzM21weXJjb3hzOTMifQ.D7oqQqX-t5YlZ5CeOnCVvQ'
-}).addTo(mymap);
-
-function popupMsg (msg, coords) {
-  L.popup()
-  .setLatLng(coords)
-  .setContent(msg)
-  .openOn(mymap);
-}
 
 function searchInput () {
   let input = document.querySelector('input').value;
@@ -70,7 +54,7 @@ function getNpProjects(stateAbvr, arr) {
   let stateObj = arr.find(function (obj) {
     if (!(obj.state === stateAbvr)) {
       let msg = 'This state has no National Parks being improved.'
-      popupMsg(msg, [45, -98.0]);
+      maps.popupMsg(msg, [45, -98.0]);
       return null;
     }
     else {
@@ -96,7 +80,6 @@ return {
   stateAbvr: stateAbvr,
   getNpProjects: getNpProjects,
   messager: messager,
-  popupMsg: popupMsg
 }
 
 })();
---
+++ js/maps.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var maps = (function() {
+
+  var mymap = L.map('map', {
+    center: [50, -98],
+    zoom: 3,
+    minZoom: 3,
+    zoomControl: false
+  });
+
+  // var mymap = L.map('map').setView([50, -98.0], 3);
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 50,
+      id: 'mapbox.outdoors',
+      accessToken: 'pk.eyJ1IjoibWF0c2FkIiwiYSI6ImNpdWZyMGp6ZTAwaHkzM21weXJjb3hzOTMifQ.D7oqQqX-t5YlZ5CeOnCVvQ'
+  }).addTo(mymap);
+
+  function popupMsg (msg, coords) {
+    L.popup()
+    .setLatLng(coords)
+    .setContent(msg)
+    .openOn(mymap);
+    mymap.setView(coords, 5);
+    // mymap.center(coords);
+    // panTo(coords);
+  }
+
+  return {
+    popupMsg: popupMsg
+  }
+})();
